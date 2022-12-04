2f449a5d7babf12d9eef0330ae178b3de1e531a0
HUSEYIN6134 <huseyin.murat@st.uskudar.edu.tr>
2022-12-04 14:53:59 +0300

    MethodOverloading
    Engin Demiroğ Java Kursu

---
+++ src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		DörtIslem dörtişlem = new DörtIslem();
+		System.out.println(dörtişlem.topla(2,3));
+		System.out.println(dörtişlem.topla(2,3,5));
+
+	}
+
+}
